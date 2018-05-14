2d5d108044510bcb9cb2dc925a6e2fc5dc57fd32
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-14 23:48:56 +0300

    upd dto and constraints

--- src/main/java/com/loopme/api/controller/dto/SignUpRequest.java
+++ src/main/java/com/loopme/api/controller/dto/SignUpRequest.java
@@ -1,25 +1,28 @@
 package com.loopme.api.controller.dto;
 
+import com.loopme.api.model.UserRole;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@ToString
 @Getter
 @Setter
 public class SignUpRequest {
     private static final String regExp = "^.+@.+\\..+$";
 
-    @NotBlank
     @NotNull
+    @NotBlank(message = "user name can not be blank")
     @Size(message = "user name size must be between 3 and 100 characters", min = 4, max = 100)
     private String name;
 
     @NotNull
-    @NotBlank
+    @NotBlank(message = "user email can not be blank")
     @Email(message = "please provide a valid email address", regexp = regExp)
     @Size(message = "user email size must be between 5 and 100 characters", min = 3, max = 100)
     private String email;
@@ -28,4 +31,7 @@ public class SignUpRequest {
     @NotBlank(message = "Please enter your password")
     @Size(message = "user password lengths must be higher or equals 8", min = 6, max = 255)
     private String password;
+
+    @NotBlank(message = "user role can not be blank")
+    private UserRole role;
 }
