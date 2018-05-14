277a364b8d629bc0d73e5b9e4b244ae0bb7b6fb0
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-14 13:19:48 +0300

    added init db

---
+++ src/main/java/com/loopme/api/populator/Init.java
@@ -0,0 +1,41 @@
+package com.loopme.api.populator;
+
+import com.loopme.api.model.User;
+import com.loopme.api.model.UserRole;
+import com.loopme.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class Init {
+    private static final String USER_NAME = "oleg";
+    private static final String USER_PASSWORD = "qwerty78";
+    private static final String USER_EMAIL = "snake@gmail.com";
+    private static final UserRole USER_ROLE = UserRole.ROLE_ADMIN;
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @PostConstruct
+    public void init() {
+        List<User> all = userRepository.findAll();
+        if (all.size() == 0) {
+            User user = User.builder()
+                    .name(USER_NAME)
+                    .password(encoder.encode(USER_PASSWORD))
+                    .email(USER_EMAIL)
+                    .role(USER_ROLE)
+                    .apps(Collections.emptySet())
+                    .build();
+
+            userRepository.save(user);
+        }
+    }
+}
