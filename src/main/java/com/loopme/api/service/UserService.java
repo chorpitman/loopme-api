b58284b5f2b176e0a531482f4645978d268670bc
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-14 23:50:31 +0300

    added upd and delete user methods with impl. added role validator

--- src/main/java/com/loopme/api/service/UserService.java
+++ src/main/java/com/loopme/api/service/UserService.java
@@ -2,12 +2,14 @@ package com.loopme.api.service;
 
 import com.loopme.api.controller.dto.SignUpRequest;
 import com.loopme.api.controller.dto.UserDto;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.loopme.api.controller.dto.UserUpdDto;
 
 public interface UserService {
     String getCurrentAuthUser();
 
     UserDto createUser(final SignUpRequest signUpRequest);
 
-    UserDto update(final UserDto userDto);
+    UserDto update(final Long userId, final UserUpdDto userDto);
+
+    Boolean delete(final Long userId);
 }
--- src/main/java/com/loopme/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/loopme/api/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.loopme.api.service.impl;
 
 import com.loopme.api.controller.dto.SignUpRequest;
 import com.loopme.api.controller.dto.UserDto;
+import com.loopme.api.controller.dto.UserUpdDto;
 import com.loopme.api.converter.UserConverter;
 import com.loopme.api.model.User;
 import com.loopme.api.model.UserRole;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.Objects;
@@ -24,19 +26,20 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     @Override
     public UserDto createUser(final SignUpRequest signUpRequest) {
-        //todo add logger
         User foundUser = userRepository.findByEmail(signUpRequest.getEmail());
         if (Objects.nonNull(foundUser)) {
-            throw new IllegalArgumentException();
+            return null;
         }
-
+        //valid role
+        validRole(signUpRequest.getRole());
         User user = User.builder()
                 .name(signUpRequest.getName())
                 .email(signUpRequest.getEmail())
                 .password(encoder.encode(signUpRequest.getPassword()))
-                .role(UserRole.ROLE_PUBLISHER)
+                .role(signUpRequest.getRole())
                 .apps(Collections.emptySet())
                 .build();
 
@@ -45,12 +48,40 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDto update(UserDto userDto) {
-        return null;
+    public UserDto update(final Long userId, final UserUpdDto userDto) {
+        User foundUser = userRepository.findOne(userId);
+        foundUser.setName(userDto.getName());
+        foundUser.setEmail(userDto.getEmail());
+
+        return userConverter.convert(foundUser);
+    }
+
+    @Override
+    public Boolean delete(final Long userId) {
+        User foundUser = userRepository.findOne(userId);
+        if (Objects.isNull(foundUser)) {
+            return Boolean.FALSE;
+        }
+        userRepository.delete(userId);
+        return Boolean.TRUE;
     }
 
     @Override
     public String getCurrentAuthUser() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    private void validRole(final UserRole userRole) {
+        User currentUser = userRepository.findByEmail(getCurrentAuthUser());
+        //админа никто не может создавать
+        if (Objects.equals(UserRole.ROLE_ADMIN, userRole)) {
+            //todo add custom exception or else
+            throw new IllegalArgumentException();
+        }
+        //если текущий юзер оператор, то создавать оператора он не может
+        if (Objects.equals(UserRole.ROLE_ADOPS, currentUser.getRole())) {
+            //todo add custom exception or else
+            throw new IllegalArgumentException();
+        }
     }
 }
