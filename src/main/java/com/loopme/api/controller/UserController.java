4b0362075b55367a976c01de8bf7d6496c2776c6
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-14 23:49:36 +0300

    added logging and methos upd, delete

--- src/main/java/com/loopme/api/controller/UserController.java
+++ src/main/java/com/loopme/api/controller/UserController.java
@@ -2,12 +2,16 @@ package com.loopme.api.controller;
 
 import com.loopme.api.controller.dto.SignUpRequest;
 import com.loopme.api.controller.dto.UserDto;
+import com.loopme.api.controller.dto.UserUpdDto;
 import com.loopme.api.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,31 +22,50 @@ import java.util.Objects;
 @Controller
 @RequestMapping("/api")
 public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    private final UserService userService;
 
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-    //create publisher
     @RequestMapping(value = "/user", method = RequestMethod.POST)
-    @PreAuthorize(value = "hasRole('ADMIN')")
+    @PreAuthorize(value = "hasAnyRole('ADMIN, ADOPS')")
     public ResponseEntity<UserDto> createUser(@RequestBody @Valid final SignUpRequest user) {
-//        LOGGER.debug("About process get user profile by id '{}'", userId);
-        String currentAuthUser = userService.getCurrentAuthUser();
+        LOGGER.info(">>> About registration process '{}'", user);
+
         UserDto createdUser = userService.createUser(user);
         if (Objects.isNull(createdUser)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            LOGGER.error(">>> Unable to create. A User with name {} already exist", user.getName());
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
-    //upd publisher
-//    @RequestMapping(value = "/user", method = RequestMethod.PUT)
-//    public ResponseEntity<Void> updateUser(@RequestBody @Valid final UserDto dto) {
-////        LOGGER.debug("About process get user profile by id '{}'", userId);
-////        User user = userService.createUser();
-//        if (Objects.isNull(user)) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-//        }
-//        return ResponseEntity.status(HttpStatus.CREATED).build();
-//    }
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+    @PreAuthorize(value = "hasAnyRole('ADMIN, ADOPS')")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") final Long id, @RequestBody @Valid final UserUpdDto user) {
+        LOGGER.info(">>> About update process '{}'", user);
+
+        UserDto updatedUser = userService.update(id, user);
+        if (Objects.isNull(updatedUser)) {
+            LOGGER.error(">>> Unable to update. User with id {} not found.", id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    @PreAuthorize(value = "hasAnyRole('ADMIN, ADOPS')")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") final Long id) {
+        LOGGER.info(">>> About user delete process by id '{}'", id);
+
+        if (!userService.delete(id)) {
+            LOGGER.error(">>> Unable to delete. User with id {} not found.", id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
