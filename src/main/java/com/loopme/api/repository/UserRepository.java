08361a4c9342b1c36839fb7cb2a2f4f691237e71
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-14 23:49:48 +0300

    removed redundant

--- src/main/java/com/loopme/api/repository/UserRepository.java
+++ src/main/java/com/loopme/api/repository/UserRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByName(final String name);
-
     User findByEmail(final String email);
 }
