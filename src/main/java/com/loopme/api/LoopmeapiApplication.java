36b7304ac998781a0d5cbc5d55f66a3677cbac1a
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-14 13:20:39 +0300

    added start app and test classes

---
+++ src/main/java/com/loopme/api/LoopmeapiApplication.java
@@ -0,0 +1,12 @@
+package com.loopme.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopmeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopmeapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/loopme/api/loopmeapi/LoopmeapiApplicationTests.java
@@ -0,0 +1,16 @@
+//package com.loopme.api.loopmeapi;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class LoopmeapiApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
