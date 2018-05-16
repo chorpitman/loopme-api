a04983535278d88e5b5f5563e0ad26689113cb59
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-16 14:32:45 +0300

    added getOperationAuthor()

--- src/main/java/com/loopme/api/service/AuthenticationService.java
+++ src/main/java/com/loopme/api/service/AuthenticationService.java
@@ -1,8 +1,11 @@
 package com.loopme.api.service;
 
+import com.loopme.api.model.User;
 import org.springframework.security.core.Authentication;
 
 public interface AuthenticationService {
 
     Authentication getAuthentication();
+
+    User getOperationAuthor();
 }
--- src/main/java/com/loopme/api/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/loopme/api/service/impl/AuthenticationServiceImpl.java
@@ -1,15 +1,31 @@
 package com.loopme.api.service.impl;
 
+import com.loopme.api.model.User;
+import com.loopme.api.repository.UserRepository;
 import com.loopme.api.service.AuthenticationService;
+import com.loopme.api.service.UserUtilService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserUtilService userUtilService;
 
     @Override
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @Override
+    public User getOperationAuthor() {
+        User foundUser = userRepository.findByEmail(getAuthentication().getName());
+        userUtilService.nullCheck(foundUser, getAuthentication().getName());
+
+        return foundUser;
     }
 }
