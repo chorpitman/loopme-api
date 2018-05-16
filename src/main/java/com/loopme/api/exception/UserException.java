4c81d5934ed57e6f6417928d90522c124059f2a4
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-16 12:21:54 +0300

    custom exception

---
+++ src/main/java/com/loopme/api/exception/UserException.java
@@ -0,0 +1,19 @@
+package com.loopme.api.exception;
+
+public class UserException extends RuntimeException {
+    public UserException() {
+        super();
+    }
+
+    public UserException(String message) {
+        super(message);
+    }
+
+    public UserException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
