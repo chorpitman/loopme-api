0427b0ff3efdc175dfdd2772bd51ef3403be1d1f
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-16 16:20:07 +0300

    added converter from List<App> apps to List<AppDto>

--- src/main/java/com/loopme/api/converter/AppConverter.java
+++ src/main/java/com/loopme/api/converter/AppConverter.java
@@ -4,6 +4,9 @@ import com.loopme.api.controller.dto.app.AppDto;
 import com.loopme.api.model.App;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class AppConverter {
     public AppDto convert(final App app) {
@@ -13,5 +16,15 @@ public class AppConverter {
                 .type(app.getType())
                 .contentTypes(app.getContentTypes())
                 .build();
+    }
+
+    public List<AppDto> convert(final List<App> apps) {
+        List<AppDto> appDtoList = new ArrayList<>();
+        for (App app : apps) {
+            AppDto converted = convert(app);
+            appDtoList.add(converted);
+        }
+
+        return appDtoList;
     }
 }
