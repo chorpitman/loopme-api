509a899f8e6b59666fefe849a67415b92e4d5523
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-16 12:23:20 +0300

    added find by id and find all endpoints

--- src/main/java/com/loopme/api/controller/UserController.java
+++ src/main/java/com/loopme/api/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Objects;
 
 @Controller
@@ -38,7 +39,7 @@ public class UserController {
 
         UserDto createdUser = userService.createUser(user);
         if (Objects.isNull(createdUser)) {
-            LOGGER.error(">>> Unable to create. A User with name {} already exist", user.getName());
+            LOGGER.error(">>> Unable to create. A User with name: {} already exist", user.getName());
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
@@ -51,7 +52,7 @@ public class UserController {
 
         UserDto updatedUser = userService.update(id, user);
         if (Objects.isNull(updatedUser)) {
-            LOGGER.error(">>> Unable to update. User with id {} not found.", id);
+            LOGGER.error(">>> Unable to update. User with id: {} not found.", id);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -60,12 +61,36 @@ public class UserController {
     @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
     @PreAuthorize(value = "hasAnyRole('ADMIN, ADOPS')")
     public ResponseEntity<Void> deleteUser(@PathVariable("id") final Long id) {
-        LOGGER.info(">>> About user delete process by id '{}'", id);
+        LOGGER.info(">>> About user delete process by id: '{}'", id);
 
-        if (!userService.delete(id)) {
-            LOGGER.error(">>> Unable to delete. User with id {} not found.", id);
+        if (userService.delete(id)) {
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        LOGGER.error(">>> Unable to delete. User with id: {} not found.", id);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    @PreAuthorize(value = "hasAnyRole('ADMIN, ADOPS')")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") final Long id) {
+        LOGGER.info(">>> About get user by id: '{}'", id);
+        UserDto user = userService.findById(id);
+        if (Objects.isNull(user)) {
+            LOGGER.error(">>> User with id {} not found.", id);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
+
+    @RequestMapping(value = "/user/", method = RequestMethod.GET)
+    @PreAuthorize(value = "hasAnyRole('ADMIN, ADOPS')")
+    public ResponseEntity<List<UserDto>> getUsers() {
+        LOGGER.info(">>> About get all users");
+        List<UserDto> users = userService.findAll();
+        if (users.size() == 0) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
 }
