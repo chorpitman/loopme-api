edf08e0c4b5b2fffb977ec1b7a626f244d665ad2
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-16 22:58:51 +0300

    upd init

--- src/main/java/com/loopme/api/populator/Init.java
+++ src/main/java/com/loopme/api/populator/Init.java
@@ -1,5 +1,8 @@
 package com.loopme.api.populator;
 
+import com.loopme.api.model.App;
+import com.loopme.api.model.AppType;
+import com.loopme.api.model.ContentType;
 import com.loopme.api.model.User;
 import com.loopme.api.model.UserRole;
 import com.loopme.api.repository.UserRepository;
@@ -8,15 +11,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class Init {
-    private static final String USER_NAME = "oleg";
-    private static final String USER_PASSWORD = "qwerty78";
-    private static final String USER_EMAIL = "snake@gmail.com";
-    private static final UserRole USER_ROLE = UserRole.ROLE_ADMIN;
+    private static final String ADMIN_NAME = "john";
+    private static final String ADMIN_EMAIL = "john@gmail.com";
+    private static final UserRole ADMIN_ROLE = UserRole.ROLE_ADMIN;
+
+    private static final String ADOP_NAME = "jack";
+    private static final String ADOP_EMAIL = "jack@gmail.com";
+    private static final UserRole ADOP_ROLE = UserRole.ROLE_ADOPS;
+
+    private static final String PUBLISHER_NAME = "jim";
+    private static final String PUBLISHER_EMAIL = "jim@gmail.com";
+    private static final UserRole PUBLISHER_ROLE = UserRole.ROLE_PUBLISHER;
+
+    private static final String PASSWORD = "qwerty78";
 
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
@@ -31,15 +45,55 @@ public class Init {
     public void init() {
         List<User> all = userRepository.findAll();
         if (all.size() == 0) {
-            User user = User.builder()
-                    .name(USER_NAME)
-                    .password(encoder.encode(USER_PASSWORD))
-                    .email(USER_EMAIL)
-                    .role(USER_ROLE)
-                    .apps(Collections.emptySet())
+            User admin = User.builder()
+                    .name(ADMIN_NAME)
+                    .password(encoder.encode(PASSWORD))
+                    .email(ADMIN_EMAIL)
+                    .role(ADMIN_ROLE)
+                    .apps(getApp(ADOP_NAME))
                     .build();
 
-            userRepository.save(user);
+            //create another user
+            User operator = User.builder()
+                    .name(ADOP_NAME)
+                    .password(encoder.encode(PASSWORD))
+                    .email(ADOP_EMAIL)
+                    .role(ADOP_ROLE)
+                    .apps(getApp(ADOP_NAME))
+                    .build();
+
+            //create another user
+            User publisher = User.builder()
+                    .name(PUBLISHER_NAME)
+                    .password(encoder.encode(PASSWORD))
+                    .email(PUBLISHER_EMAIL)
+                    .role(PUBLISHER_ROLE)
+                    .apps(getApp(PUBLISHER_NAME))
+                    .build();
+
+            //save
+            List<User> users = Arrays.asList(admin, operator, publisher);
+            userRepository.save(users);
         }
     }
+
+    private Set<App> getApp(final String name) {
+        Set<App> apps = new HashSet<>();
+        apps.add(App.builder()
+                .name("application " + name)
+                .type(AppType.IOS)
+                .contentTypes(getContentType())
+                .build());
+        return apps;
+    }
+
+    private Set<ContentType> getContentType() {
+        Set<ContentType> contentTypes = new HashSet<>();
+        contentTypes.add(ContentType.VIDEO);
+        contentTypes.add(ContentType.HTML);
+        contentTypes.add(ContentType.IMAGE);
+
+        return getContentType();
+    }
+
 }
