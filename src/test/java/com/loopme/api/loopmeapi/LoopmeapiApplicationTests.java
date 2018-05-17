94ccff66b3c9f17e1a698ad957172244b657e941
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-17 12:53:48 +0300

    removed redundant

--- src/test/java/com/loopme/api/loopmeapi/LoopmeapiApplicationTests.java
+++ src/test/java/com/loopme/api/loopmeapi/LoopmeapiApplicationTests.java
@@ -1,16 +1,10 @@
-//package com.loopme.api.loopmeapi;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class LoopmeapiApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package com.loopme.api.loopmeapi;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoopmeapiApplicationTests {
+}
