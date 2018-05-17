9abc42ebf23da3160ffcd40f1114c87788ca8705
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-05-17 14:27:45 +0300

    changed main class name

--- src/main/java/com/loopme/api/Application.java
+++ src/main/java/com/loopme/api/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoopmeapiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoopmeapiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
